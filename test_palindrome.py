457def4460138d803d43b3ac95e252bdffd8fa21
Sofía Denner <sofia.denner@mercadolibre.com>
2020-11-23 19:03:14 -0300

    fix tests and add example

--- test_palindrome.py
+++ test_palindrome.py
@@ -1,6 +1,6 @@
 import pytest
 
-from palindrome import insertions_to_make_palindrome
+from palindrome import insertions_to_palindrome
 
 
 PALINDROME_CASES = [
@@ -11,7 +11,8 @@ PALINDROME_CASES = [
     ("11223322", 2),
     ("0123456789a", 10),
     ("FFT", 1),
+    ("1235676541", 3),  # missing 4 from the left and 3 and 2 from the right
 ]
 @pytest.mark.parametrize("text,insertions", PALINDROME_CASES)
 def test_palindrome(text, insertions):
-    assert insertions_to_make_palindrome(text) == insertions
+    assert insertions_to_palindrome(text) == insertions
